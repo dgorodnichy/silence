7911ce598ebfb81e84c51f883544a4ed83d4f63a
gorodni4ij <gorodni4ij@gmail.com>
2015-04-19 23:11:27 +0300

    add project structure

--- lib/silence/cli.rb
+++ lib/silence/cli.rb
@@ -8,14 +8,14 @@ module Silence
     option :upcase
 
     def new(name)
-      project_directories = ["features", 
-                             "config", 
-                             "reports",
-                             "features/pages",
-                             "features/step_definitions" ]
+      #project_directories = ["features", 
+                             #"config", 
+                             #"reports",
+                             #"features/pages",
+                             #"features/step_definitions" ]
 
-      Dir.mkdir(name) unless File.exists?(name)
-      create_directories(name, project_directories)
+      #Dir.mkdir(name) unless File.exists?(name)
+      create_directories(name)
     end
 
     no_commands do
@@ -24,16 +24,23 @@ module Silence
           printf "\033[#{color}m#{string}\033[0m\n"
       end
 
-      def create_directories(name, directories)
-        directories.each do |directory| 
-          path = "#{name}/#{directory}"
-          unless File.exists?(path)
-            Dir.mkdir(path) 
-            color_output("Create path: #{path}", 32)
-          else
-            color_output("Skipped: #{path}. Path already exists", 33)
-          end
-        end
+      def create_directories(name)
+        spec = Gem::Specification.find_by_name 'silence'       
+        p "#{spec.gem_dir}"
+        p Dir.pwd
+
+        FileUtils.cp_r "#{spec.gem_dir}/lib/.", Dir.pwd, :verbose => true
+        #FileUtils.cp_r "source/.", 'dst', :verbose => true
+
+        #directories.each do |directory| 
+          #path = "#{name}/#{directory}"
+          #unless File.exists?(path)
+            #Dir.mkdir(path) 
+            #color_output("Create path: #{path}", 32)
+          #else
+            #color_output("Skipped: #{path}. Path already exists", 33)
+          #end
+        #end
       end
 
     end
