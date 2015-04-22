860e7e2113dbba402f2398d0d4063c26a31073c6
gorodni4ij <gorodni4ij@gmail.com>
2015-04-22 22:41:34 +0300

    setup ruby and gemset automatically

--- lib/silence/cli.rb
+++ lib/silence/cli.rb
@@ -9,8 +9,9 @@ module Silence
 
     def new(name)
       Dir.mkdir(name) unless File.exists?(name)
-      spec = Gem::Specification.find_by_name 'silence'       
-      FileUtils.cp_r "#{spec.gem_dir}/lib/.", "#{Dir.pwd}/#{name}", :verbose => true
+      copy_project_structure
+      setup_ruby_version("ruby-2.2.0")
+      setup_gemset(name)
       color_output(system("rvm gemset list"), 34)
       system("cd #{name} && bundle")
     end
@@ -19,6 +20,19 @@ module Silence
 
       def color_output(string, color)
         printf "\033[#{color}m#{string}\033[0m\n"
+      end
+
+      def setup_gemset(name)
+        File.new("#{name}/.ruby-gemset") { |file| file.puts(name) }
+      end
+
+      def setup_version(version)
+        File.new("#{name}/.ruby-version") { |file| file.puts(version) }
+      end
+
+      def copy_project_structure
+        spec = Gem::Specification.find_by_name 'silence'       
+        FileUtils.cp_r "#{spec.gem_dir}/lib/.", "#{Dir.pwd}/#{name}", :verbose => true
       end
 
     end
